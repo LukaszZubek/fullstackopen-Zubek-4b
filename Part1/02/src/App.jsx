c4530de7ca204b8d64949d1e6508f22da687c2a7
LukaszZubek <zubeklukasz2656@gmail.com>
2025-05-21 22:36:28 +0200

    1.8

--- Part1/02/src/App.jsx
+++ Part1/02/src/App.jsx
@@ -1,51 +1,48 @@
 import { useState } from 'react'
 
+const Stats = ({vars}) => {
+  return(
+    <div>
+      <p>good {vars[0]}</p>
+      <p>neutral {vars[1]}</p>
+      <p>bad {vars[2]}</p>
+      <p>all {vars[3]}</p>
+      <p>average {(vars[0] + vars[2]) / vars[3]}</p>
+      <p>positive {(vars[0] / vars [3]) * 100}%</p>
+    </div>
+  )
+}
+const Header = ({text}) => {
+  return(
+    <h1>
+      {text}
+    </h1>
+  )
+}
+
+const FeedbackButton = ({variable, text}) => {
+  return(
+    <button onClick={variable}>
+      {text}
+    </button>
+  )
+}
+
+const FeedbackButtons = ({vars}) => {
+  return(
+    <div>
+      <FeedbackButton variable={vars[0]} text="good"/>
+      <FeedbackButton variable={vars[1]} text="neutral"/>
+      <FeedbackButton variable={vars[2]} text="bad"/>
+    </div>
+  )
+}
 const App = () => {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
   const [votes, setVotes] = useState(0)
-
-
-  const Header = ({text}) => {
-    return(
-      <h1>
-        {text}
-      </h1>
-    )
-  }
-
-  const FeedbackButton = ({variable, text}) => {
-    return(
-      <button onClick={variable}>
-        {text}
-      </button>
-    )
-  }
-
-  const FeedbackButtons = ({vars}) => {
-    return(
-      <div>
-        <FeedbackButton variable={vars[0]} text="good"/>
-        <FeedbackButton variable={vars[1]} text="neutral"/>
-        <FeedbackButton variable={vars[2]} text="bad"/>
-      </div>
-    )
-  }
-
-  const Stats = ({vars}) => {
-    return(
-      <div>
-        <p>good {vars[0]}</p>
-        <p>neutral {vars[1]}</p>
-        <p>bad {vars[2]}</p>
-        <p>all {vars[3]}</p>
-        <p>average {(vars[0] + vars[2]) / vars[3]}</p>
-        <p>positive {(vars[0] / vars [3]) * 100}%</p>
-      </div>
-    )
-  }
 
   return (
     <div>
