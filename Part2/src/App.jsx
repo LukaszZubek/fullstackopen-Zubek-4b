a661ae5191a06989e601d1ee537e569d335d5f64
LukaszZubek <zubeklukasz2656@gmail.com>
2025-05-24 23:44:38 +0200

    2.7

--- Part2/src/App.jsx
+++ Part2/src/App.jsx
@@ -3,22 +3,34 @@ import Content from './components/Content'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      phone: '780 213 458'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   const handleNameImput = (event) => {
     setNewName(event.target.value)
   }
+  const handlePhoneImput = (event) => {
+    setNewPhone(event.target.value)
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
-    if(!persons.some(persons => persons.name === newName))
+    if(!persons.some(person => person.name === newName))
       {
-        const newPerson = {
-          name: newName
+        if(!persons.some(person => person.phone === newPhone)){
+          const newPerson = {
+            name: newName,
+            phone: newPhone
+          }
+          setPersons(persons.concat(newPerson))
         }
-        setPersons(persons.concat(newPerson))
+        else
+          alert(`${newPhone} is already added to phonebook`)
       }
     else
       alert(`${newName} is already added to phonebook`)
@@ -31,6 +43,9 @@ const App = () => {
       <form>
         <div>
           name: <input value={newName} onChange={handleNameImput}/>
+        </div>
+        <div>
+          phone: <input value={newPhone} onChange={handlePhoneImput}/>
         </div>
         <div>
           <button type="submit" onClick={addPerson}>add</button>
--- Part2/src/components/Content.jsx
+++ Part2/src/components/Content.jsx
@@ -3,7 +3,7 @@ import Person from './Person'
 const Content = ({persons}) => {
     return(
         <div>
-            {persons.map(person => <Person key={person.name} name={person.name} />)}
+            {persons.map(person => <Person key={person.name} name={person.name} phone={person.phone} />)}
         </div>
     )
 }
