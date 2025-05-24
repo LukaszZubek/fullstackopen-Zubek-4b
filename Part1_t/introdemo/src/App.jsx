c9bafc0b76351c249dba3fe6be788623e38a2ba3
LukaszZubek <zubeklukasz2656@gmail.com>
2025-05-24 21:17:18 +0200

    2.1

--- Part1_t/introdemo/src/App.jsx
+++ Part1_t/introdemo/src/App.jsx
@@ -1,36 +1,27 @@
-const Hello = () => {
-  console.log("hello world");
-}
-
-const Cprint = (tekst) => {
-  console.log(tekst.toString())
-}
-
-const Print = (tekst) => {
-  return(
-    <div>
-      <p>{tekst.name}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
 
 const App = () => {
-  console.log("dziala")
-  const now = Date.now();
-  const a = 2, b = 4;
-  console.log("dzisiaj jest " + now.toString());
-  console.log(a + " + " + b + " = " + a+b);
-  Cprint("joł joł joł");
-  return(
-    <div>
-      <p>Hello World</p>
-      <p>Dzisiaj jest {new Date(now).getDate().toString()}.{(new Date(now).getMonth() + 1).toString()}.{new Date(now).getFullYear().toString()}</p>
-      <input id="tetetekst"></input>
-      <button id="tetetekst_btn">SEND</button>
-      <p>{a} + {b} = {a+b}</p>
-      <Hello/>
-      <Print name="yo yo yo"/>
-    </div>
+
+  const [ counter, setCounter ] = useState(0)
+
+  const handleClick = () => {
+    setCounter(counter + 1)
+    console.log("+1")
+  }
+
+  const handleReset = () => {
+    setCounter(0)
+    console.log("reset")
+  }
+
+  return (
+    <center>
+      <div>
+        {counter}
+        <p><button onClick={handleClick}>PLUS 1</button></p>
+        <p><button onClick={handleReset}>RESET</button></p>
+      </div>
+    </center>
   )
 }
 
