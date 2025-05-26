4d17119e52f8d25cdfb8e5512a8a80dea4b6f643
LukaszZubek <zubeklukasz2656@gmail.com>
2025-05-26 20:11:06 +0200

    2.10

--- Part2/src/components/Content.jsx
+++ Part2/src/components/Content.jsx
@@ -1,14 +1,8 @@
-import Person from './Person'
+import Search from './Search'
 
 const Content = ({persons, search}) => {
     return(
-        <div>
-            {
-                (search.trim() != '')
-                ? persons.filter(person => (person.name.toLowerCase().includes(search) || person.phone.includes(search))).map(person => <Person key={person.name} name={person.name} phone={person.phone} />)
-                : persons.map(person => <Person key={person.name} name={person.name} phone={person.phone} />)
-            }
-        </div>
+        <Search persons={persons} search={search} />
     )
 }
 
---
+++ Part2/src/components/Search.jsx
@@ -0,0 +1,15 @@
+import Person from './Person'
+
+const Search = ({persons, search}) => {
+    return(
+        <div>
+            {
+                (search.trim() != '')
+                ? persons.filter(person => (person.name.toLowerCase().includes(search) || person.phone.includes(search))).map(person => <Person key={person.name} name={person.name} phone={person.phone} />)
+                : persons.map(person => <Person key={person.name} name={person.name} phone={person.phone} />)
+            }
+        </div>
+    )
+}
+
+export default Search
